e78411d984d9a2068363ce1240ba1cca7bef7714
pnpi <fargosog@gmail.com>
2024-07-04 16:21:40 -0400

    gEnv init
    Global environment for real-time configuration.

--- Genv.lua
+++ Genv.lua
@@ -1,1 +1,50 @@
 -- Global environment for real-time configuration.
+
+getgenv().Tsundere = {
+    AimSettings = {
+        FieldOfView = {
+            Color = Color3.fromRGB(0, 255, 255),
+            Radius = 150,
+            Filled = false,
+            Visible = true,
+            PositionMode = "Mouse", -- Mouse: Relative to mouse location -> Point: Relative to part position 
+            Thickness = 1,
+            Transparency = 1,
+        },
+
+        Validations = {
+            Raycast = true, -- Ensure the part is visible
+            ResetVelocity = true, -- Ensure the part's position is above the surface level of the map
+        },
+
+        Smoothing = {
+            Key = Enum.UserInputType.MouseButton2,
+            Mode = "Toggle", 
+            TargetParts = {"Head", "UpperTorso"},
+            Speed = 0,
+            EasingStyle = "Sine",
+            EasingDirection = "InOut",
+            CurveOffset = 0,
+            MovementOffset = 0,
+        }
+    },
+
+    MiscSettings = {
+        Skybox = {
+            Enabled = true,
+            Theme = "Celestial"
+        },
+
+        Rotation = {
+            Enabled = false, 
+            Key = Enum.KeyCode.K,
+            Speed = 2
+        },
+
+        CustomCursor = {
+            Enabled = false, 
+            Theme = "2014",
+            Scale = 75
+        }
+    }
+}
